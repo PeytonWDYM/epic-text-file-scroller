9de7bb2c3dfaa01899cfdaa82ae3d4b4541ba0e5
Peyton <peytonvlogs34@gmail.com>
2023-11-03 18:28:37 -0500

    modified:   public/index.js renamed:    public/example.txt -> public/text.txt

--- public/index.js
+++ public/index.js
@@ -8,7 +8,7 @@ let bufferText = ""
 let newText = ""
 
 function getAlertText() {
-    fetch("example.txt")
+    fetch("text.txt")
     .then(response => {
         if (!response.ok) {
           throw new Error('Network response was not ok ' + response.statusText);
